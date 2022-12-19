38359e7988e64f5314437c82d5ef785b0c29d805
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-19 00:08:50 -0300

    feat: added globalstyle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
 import type { AppProps } from 'next/app';
+import { GlobalStyle } from '../styles/global';
+import '../styles/_app.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  );
 }
