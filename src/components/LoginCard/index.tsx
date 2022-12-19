5d6d1ec272bf39fcebae1685cdd5b13b728ed97a
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-19 00:19:43 -0300

    feat: login page design done

--- src/components/LoginCard/index.tsx
+++ src/components/LoginCard/index.tsx
@@ -4,6 +4,11 @@ import * as S from './styles';
 import logo from '../../assets/devchallenges.svg';
 import Form from '../Form';
 
+import google from '../../assets/Google.svg';
+import facebook from '../../assets/Facebook.svg';
+import twitter from '../../assets/Twitter.svg';
+import github from '../../assets/Github.svg';
+
 const LoginCard = () => {
   return (
     <>
@@ -14,6 +19,27 @@ const LoginCard = () => {
       <h3>Login</h3>
 
       <Form />
+
+      <S.MediaContainer>
+        <span>or continue with these social profile</span>
+
+        <S.Media>
+          <S.Icon>
+            <Image src={google} width={42} height={42} alt='google'/>
+          </S.Icon>
+          <S.Icon>
+            <Image src={facebook} width={42} height={42} alt='facebook'/>
+          </S.Icon>
+          <S.Icon>
+            <Image src={twitter} width={42} height={42} alt='twitter'/>
+          </S.Icon>
+          <S.Icon>
+            <Image src={github} width={42} height={42} alt='github'/>
+          </S.Icon>
+        </S.Media>
+      </S.MediaContainer>
+
+      <S.Text>Don&apos;t have an account yet? <a>Register</a></S.Text> {/* temporary anchor */}
     </>
   );
 };
--- src/components/LoginCard/styles.ts
+++ src/components/LoginCard/styles.ts
@@ -1,3 +1,40 @@
 import styled from 'styled-components';
 
 export const Header = styled.div``;
+
+export const MediaContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 21px;
+
+  span {
+    font-weight: 400;
+    color: #828282;
+    letter-spacing: -0.035em;
+    line-height: 19px;
+  }
+`;
+
+export const Media = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 21px;
+`;
+
+export const Icon = styled.div``;
+
+export const Text = styled.p`
+  text-align: center;
+  color: #828282;
+  letter-spacing: -0.035em;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 19px;
+
+  a {
+    color: #2D9CDB;
+  }
+`;
