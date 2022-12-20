662aff33a1ba312454ec6c9af642bb679fdad083
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-20 00:17:12 -0300

    feat: added onclick

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,12 @@ import * as S  from './styles';
 interface Props {
   children: React.ReactNode
   isEdit: boolean;
+  onClick?: () => void;
 }
 
-const Button = ({children, isEdit}: Props) => {
+const Button = ({children, isEdit, onClick}: Props) => {
   return (
-    <S.Button isEdit={isEdit}>
+    <S.Button isEdit={isEdit} type={isEdit ? 'button' : 'submit'} onClick={onClick}>
       {children}
     </S.Button>
   );
